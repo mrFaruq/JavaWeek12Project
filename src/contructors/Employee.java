42f0405a5e3f806c3861698053053e61688db6dc
omerf <omeryavuncu@gmail.com>
2022-07-24 16:58:14 +0100

    Added constructor package

---
+++ src/contructors/Employee.java
@@ -0,0 +1,35 @@
+package contructors;
+
+public class Employee {
+    public String name, jobTitle;
+    public Integer ID;
+    public double salary;               // instance variable, belongs to Object
+    public static String companyName;   //static field belongs to Class
+
+    static {
+        companyName = "Microsoft";  // this valuse is same for all the objects of this class
+    }
+
+    public Employee() {
+        salary = 10000;     //when I create an Employee object with default constructor
+    }
+
+    public Employee(String name, String jobTitle, Integer ID, double salary) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.ID = ID;
+        this.salary = salary;
+    }
+}
+/*
+Task 1:
+Employee:
+    attribute/fields/variables
+    name, jobTitle, ID, salary
+
+    add a constructor to initialize all the fields
+    add a default constructor with no fields
+
+    toString() method will print out the fields
+
+ */
